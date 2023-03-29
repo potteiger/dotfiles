ea2c0e841341fc90a57e39e1888e2fe8d1479d72
Alan Potteiger <alan@mkall.org>
2023-03-29 22:27:57 +0200

    neovim indent line plugin

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -8,19 +8,24 @@ vim.cmd([[
 	set showcmd
 	set cc=80
 	set cul
+	set cursorcolumn
 	set culopt=number,screenline
 	set tabstop=4
 	set softtabstop=4
-	set shiftwidth=4
-	set guicursor=n-v-c-sm:block,i:blinkwait300-blinkon200-blinkoff150
+	set shiftwidth=4	
 	set nu
 ]])
 
 require('lualine').setup {
 	options = { theme = 'gruvbox' }
 }
-
-vim.opt.termguicolors = true
-require("bufferline").setup{}
-
+require("bufferline").setup()
 require("scrollbar").setup()
+vim.opt.list = true
+vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "space:⋅"
+require("indent_blankline").setup {
+	show_end_of_line = true,
+	space_char_blankline = " "
+}
+
--- etc/nvim/lua/plugins.lua
+++ etc/nvim/lua/plugins.lua
@@ -19,21 +19,9 @@ return require('packer').startup(function(use)
 		'nvim-lualine/lualine.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
-
-	
-	use {
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		requires = { 
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		}
-	}
-
 	use("petertriho/nvim-scrollbar")
-
 	use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+	use "lukas-reineke/indent-blankline.nvim"
 
 	if packer_bootstrap then
 		require('packer').sync()
